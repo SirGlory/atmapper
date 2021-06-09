ebcd7ac78e802b3666f06d69fc898371ae06693c
SirGlory <29068174+SirGlory@users.noreply.github.com>
2021-06-10 00:26:28 +0200

    Database update

--- mapgen.py
+++ mapgen.py
@@ -20,12 +20,14 @@ class GenMap:
         # Generate map, cluster class and create empty coordinates list
         if self.handle == "4x4theboiz":
             my_map = Map(location=[-10, 25], zoom_start=4)
+        elif self.handle == "teampolarsteps":
+            my_map = Map(location=[52.35, 5.3], zoom_start=9)
         elif self.handle == "cityofcapetown":
             my_map = Map(location=[-34.1, 18.420], zoom_start=10)
         elif self.handle == "backpackingtours":
             my_map = Map(location=[10, 100], zoom_start=3)
         elif self.handle == "tony_ontheroad":
-            my_map = Map(location=[39.5, -1.5], zoom_start=6)
+            my_map = Map(location=[39.5, 4], zoom_start=4)
         elif self.handle == "rolling_sloane":
             my_map = Map(location=[55, 2.64], zoom_start=4)
         elif self.handle == "cape_secrets":
